9d424fb74d0876228d36931fcca5e5edb17cfa38
Oblivionys <49002672+Oblivionys@users.noreply.github.com>
2020-12-02 10:58:01 +0100

    Seq number fix

--- TCP_in_Go/server_side/serverClean.go
+++ TCP_in_Go/server_side/serverClean.go
@@ -125,8 +125,8 @@ func acceptConnection(publicConn *net.UDPConn, dataPort int) (err error) {
 
 /** takes a path to a file and sends it to the given address*/
 func sendFile(connected *bool, path string, dataConn *net.UDPConn, dataAddr net.Addr, windowSize *int) (err error) {
-	int seqNum := 0
-	
+	seqNum := 0
+
 	f, err := os.Open(path)
 	pwd, err := os.Getwd()
 	if err != nil {
@@ -159,12 +159,15 @@ func sendFile(connected *bool, path string, dataConn *net.UDPConn, dataAddr net.
 
 		//Sending fragment
 		seq := strconv.Itoa(seqNum)
-		for i<6-seqNum/10 {
-			seq="0"+seq
+		for i := 0; i < 6-seqNum/10; i++ {
+			seq = "0" + seq
 			fmt.Printf(seq)
 		}
+		byteSeq := []byte(seq)
 		fmt.Println(string(readingBuffer[0:n]))
-		_, err = dataConn.WriteTo(seq+readingBuffer[0:n], dataAddr)
+		msg := append(byteSeq, readingBuffer...)
+
+		_, err = dataConn.WriteTo(msg, dataAddr)
 		if err != nil {
 			fmt.Printf("Error sending packet %v\n", err)
 			return err

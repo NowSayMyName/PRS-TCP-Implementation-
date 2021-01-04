90c2520b5f292fcbcddb58cbce54e3f949fdbada
NowSayMyName <yoann.rouxel@insa-lyon.fr>
2021-01-04 17:29:42 +0100

    fixed last msg

--- TCP_in_Go/server_side/serverClean.go
+++ TCP_in_Go/server_side/serverClean.go
@@ -195,7 +195,7 @@ func sendFile(connected *bool, path string, dataConn *net.UDPConn, dataAddr net.
 			seqNum = 0
 		}
 	}
-	go timeCheck2(&packets, []byte("FIN"), seqNum, dataConn, dataAddr, &firstRTT)
+	// go timeCheck2(&packets, []byte("FIN"), seqNum, dataConn, dataAddr, &firstRTT)
 
 	_, err = dataConn.WriteTo([]byte("FIN"), dataAddr)
 	if err != nil {

22aa3b261cba9f9e38aee8612cc0ef3d3c7e1887
NowSayMyName <yoann.rouxel@insa-lyon.fr>
2021-01-09 22:43:29 +0100

    mtu test

--- TCP_in_Go/server_side/serverCleanPriority.go
+++ TCP_in_Go/server_side/serverCleanPriority.go
@@ -177,7 +177,7 @@ func sendFile(connected *bool, path string, dataConn *net.UDPConn, dataAddr net.
 	var mutex = &sync.Mutex{}
 
 	//variables de lecture du fichier
-	bufferSize := 1494
+	bufferSize := 2494
 	r := bufio.NewReader(f)
 	readingBuffer := make([]byte, bufferSize)
 	endOfFile := false
